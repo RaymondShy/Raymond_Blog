0879e54a723bde5e4cff9dd19e862ae64179fff9
ErYueWink <2543604745@qq.com>
2025-04-25 16:25:57 +0800

    tabs组件样式编写

--- gvb_ts/src/components/admin/gvb_tabs.vue
+++ gvb_ts/src/components/admin/gvb_tabs.vue
@@ -1,13 +1,27 @@
 <template>
   <div class="gvb_tabs">
-    <div class="gvb_tab active">首页</div>
-    <div class="gvb_tab">用户列表</div>
-    <div class="gvb_tab">文章列表</div>
+    <div :class="{gvb_tab:true, active:item.name === route.name}" v-for="item in tabList" :key="item.name">
+      {{item.title}}
+      <IconClose v-if="item.name !== 'home'"/>
+    </div>
+    <div class="gvb_tab close_all_tab">全部关闭</div>
   </div>
 </template>
 
 <script setup lang="ts">
+import {useRoute} from "vue-router";
+import {ref, type Ref} from "vue";
+import {IconClose} from "@arco-design/web-vue/es/icon";
 
+const route = useRoute()
+interface tabType{
+  name:string
+  title:string
+}
+const  tabList:Ref<tabType[]> = ref([
+  {name:'home',title:'首页'},
+  {name:'user_info',title:'我的信息'}
+])
 </script>
 
 <style lang="scss" scoped>
@@ -18,6 +32,7 @@
   align-items: center;
   padding: 0 20px;
   display: flex;
+  position: relative;
   .gvb_tab{
     border-radius: 5px;
     border: 1px solid var(--bg);
@@ -27,6 +42,22 @@
     &.active{
       background-color: var(--active);
       color: #fff;
+    }
+  }
+  .close_all_tab{
+    position: absolute;
+    right: 20px;
+    border: 1px solid var(--bg);
+    padding: 2px 6px;
+    border-radius: 5px;
+    cursor: pointer;
+  }
+  svg{
+    font-size: 12px;
+    border-radius: 50%;
+    cursor: pointer;
+    &:hover{
+      background-color: var(--bg);
     }
   }
 }
