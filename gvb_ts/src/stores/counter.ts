568272020298c00bd18f00859f6609b7577527dc
RaymondShy <raymondshy001@gmail.com>
2025-04-26 17:13:23 +0800

    主题切换

--- gvb_ts/src/stores/counter.ts
+++ gvb_ts/src/stores/counter.ts
@@ -1,9 +1,43 @@
-import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
+/* 主题切换 */
+const theme = true
 export const useCounterStore = defineStore('counter', {
   state(){
-    return{}
+    return{
+      theme: theme,
+    }
   },
-  actions: {}
+  actions: {
+    /* 主题切换 */
+    setTheme(loadTheme?:boolean){
+      if (loadTheme !== undefined){
+        this.theme = loadTheme
+      }else{
+        this.theme = !this.theme
+      }
+      document.documentElement.style.colorScheme = this.themeString
+      document.body.setAttribute('arco-theme',this.themeString)
+      localStorage.setItem('theme', JSON.stringify(this.theme))
+    },
+    /* 初始化theme */
+    loadTheme(){
+      let val = localStorage.getItem('theme')
+      if (val === null){
+        return
+      }
+        try{
+          this.theme = JSON.parse(val)
+          this.setTheme(this.theme)
+        }catch (e){
+          return;
+        }
+
+    }
+  },
+  getters:{
+    themeString():string{
+      return this.theme ? 'light' : 'dark';
+    }
+  }
 })
--- gvb_ts/src/views/admin/index.vue
+++ gvb_ts/src/views/admin/index.vue
@@ -11,9 +11,7 @@
 <!--        右侧功能区-->
         <div class="gvb_function_area">
           <IconMenu class="gvb_action"/>
-          <div class="gvb_theme">
-            <IconSun class="gvb_action"/>
-          </div>
+          <Gvb_theme/>
           <div class="gvb_menu_dropdown">
             <a-dropdown :popup-max-height="false">
               <div class="gvb_menu_dropdown_text">
@@ -55,6 +53,7 @@ import Gvb_menu from "@/components/admin/gvb_menu.vue";
 import Gvb_logo from "@/components/admin/gvb_logo.vue";
 import Gvb_bread_crumb from "@/components/admin/gvb_bread_crumb.vue";
 import Gvb_tabs from "@/components/admin/gvb_tabs.vue";
+import Gvb_theme from "@/components/admin/gvb_theme.vue";
 
 </script>
 
@@ -67,6 +66,7 @@ import Gvb_tabs from "@/components/admin/gvb_tabs.vue";
     width: 240px;
     height: 100vh;
     border-right: 1px solid var(--bg);
+    background-color: var(--color-bg-1);
   }
   main{
     width: calc(100% - 240px);
@@ -83,6 +83,15 @@ import Gvb_tabs from "@/components/admin/gvb_tabs.vue";
         align-items: center;
         .gvb_action{
           margin-right: 10px;
+        }
+        .action_icon{
+          margin-right: 10px;
+          font-size: 16px;
+          cursor: pointer;
+          transition: color .3s;
+            &:hover{
+              color: var(--active);
+            }
         }
         .gvb_menu_dropdown{
           .gvb_menu_dropdown_text{
@@ -103,6 +112,7 @@ import Gvb_tabs from "@/components/admin/gvb_tabs.vue";
       width: 100%;
       min-height: calc(100vh - 90px);
       padding: 20px;
+      background-color: var(--bg);
     }
   }
 }
